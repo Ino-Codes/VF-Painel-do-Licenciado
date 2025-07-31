738b651f91700667a919eb35413e834f8838e191
Inácio de Moraes Lindenmeyer <103116915+Ino-Codes@users.noreply.github.com>
2025-07-31 12:00:20 -0300

    alteração no estilo geral do sistema

--- frontend/src/Menu.tsx
+++ frontend/src/Menu.tsx
@@ -1,46 +1,51 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useAuth } from './context/AuthContext.tsx';
+import { useAuth } from './context/AuthContext';
 
 const Menu: React.FC = () => {
-  const { user } = useAuth();
+const { user } = useAuth();
+const defaultAvatar = 'https://res.cloudinary.com/dsgbgrll5/image/upload/v1753972686/imagem-do-usuario-com-fundo-preto_1_y0ulj0.png';
 
-  const defaultAvatar = 'https://res.cloudinary.com/dsgbgrll5/image/upload/v1753972686/imagem-do-usuario-com-fundo-preto_1_y0ulj0.png';
+return (
+<nav className="main-menu">
+  <div className="menu-left">
+    <NavLink
+      to="/dashboard"
+      className={({ isActive }) => isActive ? "menu-item active" : "menu-item"}
+    >
+      Dashboard
+    </NavLink>
 
-  return (
-    <nav className="main-menu">
-      <div className="menu-left">
-        <NavLink 
-          to="/dashboard" 
-          className={({ isActive }) => isActive ? "menu-item active" : "menu-item"}
-        >
-          Dashboard
-        </NavLink>
-        
-        {user && user.role === 'admin' && (
-          <NavLink 
-            to="/admin/users"
-            className={({ isActive }) => isActive ? "menu-item active" : "menu-item"}
-          >
-            Gerenciar Usuários
-          </NavLink>
-        )}
-      </div>
+    {user && user.role === 'admin' && (
+    <NavLink
+      to="/admin/users"
+      className={({ isActive }) => isActive ? "menu-item active" : "menu-item"}
+    >
+      Gerenciar Usuários
+    </NavLink>
+    )}
+  </div>
 
-      <div className="menu-right">
-        <NavLink 
-          to="/perfil"
-          className={({ isActive }) => isActive ? "menu-item profile-icon active" : "menu-item profile-icon"}
-        >
-          <img 
-            src={user?.avatar_url || defaultAvatar} 
-            alt="Foto do perfil" 
-            className="menu-avatar-img"
+  <div className="menu-right">
+    {user && (
+      <NavLink
+        to="/perfil"
+        className="profile-info-container"
+      >
+        <div className="profile-text">
+          <span className="profile-role">{user.role}</span>
+          <span className="profile-name">{user.nome}</span>
+        </div>
+        <div className="profile-image-container">
+          <img
+            src={user.avatar_url || defaultAvatar}
+            alt="Foto do perfil"
+            className="profile-avatar-img"
           />
-        </NavLink>
-      </div>
-    </nav>
-  );
+        </div>
+      </NavLink>
+    )}
+  </div>
+</nav>
+);
 };
-
-export default Menu;
