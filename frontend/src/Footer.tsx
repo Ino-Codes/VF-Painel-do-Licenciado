484d7bbb60f6990cb377f65113ba8dd795facdb1
Inácio de Moraes Lindenmeyer <103116915+Ino-Codes@users.noreply.github.com>
2025-08-01 17:23:22 -0300

    alteração no visual do footer

--- frontend/src/Footer.tsx
+++ frontend/src/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="main-footer">
-      <p>Valor Fiscal Inteligência Tributária</p>
+      <p>Valor Fiscal Inteligência Tributária © 2025 - Todos os direitos reservados</p>
     </footer>
   );
 };
