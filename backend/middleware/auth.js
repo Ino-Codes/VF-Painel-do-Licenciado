a702c2f2363a8f0ea0dd8c204308721da5f5a80b
VF Marketing <vfmarketing@MacBook-Pro-de-VF.local>
2025-08-18 15:04:44 -0300

    FIX: corrigindo arquivo de autenticação do middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,8 +1,11 @@
+// Este middleware verifica se o user_id enviado pertence a um usuário logado
 const isLoggedIn = (pool) => async (req, res, next) => {
   const userId = req.headers["x-user-id"];
+
   if (!userId) {
     return res.status(401).json({ error: "Autenticação necessária." });
   }
+
   try {
     const userResult = await pool.query(
       "SELECT id, role FROM users WHERE id = $1",
@@ -11,13 +14,16 @@ const isLoggedIn = (pool) => async (req, res, next) => {
     if (userResult.rowCount === 0) {
       return res.status(401).json({ error: "Usuário não encontrado." });
     }
+    // Anexa o usuário na requisição para uso posterior
     req.user = userResult.rows[0];
     next();
   } catch (err) {
+    console.error("Erro no middleware isLoggedIn:", err);
     res.status(500).json({ error: "Erro interno do servidor." });
   }
 };
 
+// Este middleware verifica se o user_id enviado pertence a um admin
 const isAdmin = (pool) => async (req, res, next) => {
   const userId = req.headers["x-user-id"];
 
@@ -38,12 +44,14 @@ const isAdmin = (pool) => async (req, res, next) => {
         .json({ error: "Acesso negado. Requer permissão de administrador." });
     }
 
+    // Anexa o usuário na requisição para uso posterior
     req.user = { id: userId, role: user.role };
     next();
   } catch (err) {
-    console.error("Erro no middleware de autenticação:", err);
+    console.error("Erro no middleware isAdmin:", err);
     res.status(500).json({ error: "Erro interno do servidor." });
   }
 };
 
-module.exports = { isAdmin };
+// Exporta ambas as funções
+module.exports = { isAdmin, isLoggedIn };
