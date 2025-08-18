4b5a268675b9eed73205644dc526b1154c6693f7
VF Marketing <vfmarketing@MacBook-Pro-de-VF.local>
2025-08-18 15:01:31 -0300

    FIX: Corrigindo rota

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { isAdmin } = require("../middleware/auth.js");
+const { isAdmin, isLoggedIn } = require("../middleware/auth.js");
 
 module.exports = function (pool) {
   const checkAdmin = isAdmin(pool);
