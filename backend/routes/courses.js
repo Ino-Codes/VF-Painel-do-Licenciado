be205708ff09373d78099f21afa9f98b80ea2c8b
Inacio <inacio@valorfiscal.com>
2025-08-22 10:29:34 -0300

    FIX: removendo biblioteca pdf-lib

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { isAdmin, isLoggedIn } = require("../middleware/auth.js");
-const { PDFDocument, rgb, StandardFonts } = require("pdf-lib");
 const puppeteer = require("puppeteer");
 
 const fetch = (...args) =>
