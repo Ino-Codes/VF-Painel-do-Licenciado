d496e1e8dc43acbc25dad15ffa3c7fe314d7ca35
Inácio de Moraes Lindenmeyer <103116915+Ino-Codes@users.noreply.github.com>
2025-09-03 11:42:43 -0300

    correção de script

--- frontend/src/CalendarPage.tsx
+++ frontend/src/CalendarPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
