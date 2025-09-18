2b1c1277e3ebf8db536b62cf09a8054390ad4759
Inácio de Moraes Lindenmeyer <103116915+Ino-Codes@users.noreply.github.com>
2025-09-18 09:26:51 -0300

    FIX: escondendo linhas de grade do eixo Y

--- frontend/src/pages/dashboard/EnneagramStats.tsx
+++ frontend/src/pages/dashboard/EnneagramStats.tsx
@@ -64,7 +64,7 @@ const EnneagramStats: React.FC = () => {
             color: getCssVar("--text-secondary"), // Cor dos números do eixo Y
           },
           grid: {
-            color: getCssVar("--bg-secondary"), // Cor das linhas de grade do eixo Y
+            color: "transparent", // Esconde as linhas de grade do eixo Y
           },
         },
         x: {
