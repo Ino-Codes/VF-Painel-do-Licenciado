0faa7d01763f19ba25593a6ed3a98acd0797da99
Inácio de Moraes Lindenmeyer <103116915+Ino-Codes@users.noreply.github.com>
2025-09-26 16:22:19 -0300

    FIX: corrigindo ordem das divs

--- frontend/src/components/layout/Menu.tsx
+++ frontend/src/components/layout/Menu.tsx
@@ -151,68 +151,68 @@ const Menu: React.FC = () => {
           FAQ
         </NavLink>
 
+        {user && user.role !== "licenciado" && (
+          <NavLink to="/empresa" className="menu-item">
+            Empresa
+          </NavLink>
+        )}
+
         {user && user.role === "admin" && (
-          <div>
-            <NavLink to="/empresa" className="menu-item">
-              Empresa
-            </NavLink>
+          <div className="dropdown-menu" ref={adminDropdownRef}>
+            <button
+              className="menu-item dropdown-trigger"
+              onClick={() => setAdminDropdownOpen(!isAdminDropdownOpen)}
+            >
+              <LockIcon />
+              Administração
+            </button>
+            {isAdminDropdownOpen && (
+              <div className="dropdown-content">
+                <NavLink
+                  to="/admin/users"
+                  className="menu-item"
+                  onClick={() => {
+                    setIsMenuOpen(false);
+                    setAdminDropdownOpen(false);
+                  }}
+                >
+                  Usuários
+                </NavLink>
 
-            <div className="dropdown-menu" ref={adminDropdownRef}>
-              <button
-                className="menu-item dropdown-trigger"
-                onClick={() => setAdminDropdownOpen(!isAdminDropdownOpen)}
-              >
-                <LockIcon />
-                Administração
-              </button>
-              {isAdminDropdownOpen && (
-                <div className="dropdown-content">
-                  <NavLink
-                    to="/admin/users"
-                    className="menu-item"
-                    onClick={() => {
-                      setIsMenuOpen(false);
-                      setAdminDropdownOpen(false);
-                    }}
-                  >
-                    Usuários
-                  </NavLink>
+                <NavLink
+                  to="/admin/courses"
+                  className="menu-item"
+                  onClick={() => {
+                    setIsMenuOpen(false);
+                    setAdminDropdownOpen(false);
+                  }}
+                >
+                  Cursos
+                </NavLink>
 
-                  <NavLink
-                    to="/admin/courses"
-                    className="menu-item"
-                    onClick={() => {
-                      setIsMenuOpen(false);
-                      setAdminDropdownOpen(false);
-                    }}
-                  >
-                    Cursos
-                  </NavLink>
+                <NavLink
+                  to="/admin/calendar"
+                  className="menu-item"
+                  onClick={() => {
+                    setIsMenuOpen(false);
+                    setAdminDropdownOpen(false);
+                  }}
+                >
+                  Agenda
+                </NavLink>
 
-                  <NavLink
-                    to="/admin/calendar"
-                    className="menu-item"
-                    onClick={() => {
-                      setIsMenuOpen(false);
-                      setAdminDropdownOpen(false);
-                    }}
-                  >
-                    Agenda
-                  </NavLink>
-
-                  <NavLink
-                    to="/admin/logs"
-                    className="menu-item"
-                    onClick={() => {
-                      setIsMenuOpen(false);
-                      setAdminDropdownOpen(false);
-                    }}
-                  >
-                    Logs
-                  </NavLink>
-                </div>
-              )}
-            </div>
+                <NavLink
+                  to="/admin/logs"
+                  className="menu-item"
+                  onClick={() => {
+                    setIsMenuOpen(false);
+                    setAdminDropdownOpen(false);
+                  }}
+                >
+                  Logs
+                </NavLink>
+              </div>
+            )}
           </div>
         )}
       </div>
