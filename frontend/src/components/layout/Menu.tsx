b944eeba27b766601be0ab13d6e28505cb0d0406
Inácio de Moraes Lindenmeyer <103116915+Ino-Codes@users.noreply.github.com>
2025-09-26 15:16:43 -0300

    FIX: Reorganizando menu em seção Documentos para comportar os Arquivos e os Documentos

--- frontend/src/components/layout/Menu.tsx
+++ frontend/src/components/layout/Menu.tsx
@@ -22,6 +22,22 @@ const LockIcon = () => (
   </svg>
 );
 
+const DocIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+  </svg>
+);
+
 const Menu: React.FC = () => {
   const { user } = useAuth();
   const defaultAvatar =
@@ -74,21 +90,34 @@ const Menu: React.FC = () => {
           Dashboard
         </NavLink>
 
-        <NavLink
-          to="/documentos"
-          className="menu-item"
-          onClick={() => setIsMenuOpen(false)}
-        >
-          Documentos
-        </NavLink>
+        <div className="dropdown-menu" ref={dropdownRef}>
+          <button
+            className="menu-item dropdown-trigger"
+            onClick={() => setAdminDropdownOpen(!isAdminDropdownOpen)}
+          >
+            <DocIcon />
+            Documentos
+          </button>
+          {isAdminDropdownOpen && (
+            <div className="dropdown-content">
+              <NavLink
+                to="/documentos"
+                className="menu-item"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Documentos
+              </NavLink>
 
-        <NavLink
-          to="/videos"
-          className="menu-item"
-          onClick={() => setIsMenuOpen(false)}
-        >
-          Vídeos
-        </NavLink>
+              <NavLink
+                to="/videos"
+                className="menu-item"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Vídeos
+              </NavLink>
+            </div>
+          )}
+        </div>
 
         <NavLink
           to="/courses"
