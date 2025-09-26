0ab81240d8c3da333fbbbfa1e80400541d3090f2
Inácio de Moraes Lindenmeyer <103116915+Ino-Codes@users.noreply.github.com>
2025-09-26 16:00:44 -0300

    FIX: ajustando importação do Empresa.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -23,6 +23,7 @@ import NotFoundPage from "./pages/public/NotFoundPage.tsx";
 import AdminCalendar from "./pages/admin/AdminCalendar.tsx";
 import EnneagramPage from "./pages/profile/EnneagramPage.tsx";
 import EnneagramResultsPage from "./pages/profile/EnneagramResultsPage.tsx";
+import Empresa from "./pages/empresa/Empresa.tsx";
 
 import "./styles/1-global.css";
 import "./styles/2-components.css";
