901233cf8ea84d334ca018fdec329a7fc7be926a
Inácio de Moraes Lindenmeyer <103116915+Ino-Codes@users.noreply.github.com>
2025-09-30 15:59:27 -0300

    FIX: corrigindo declaração dos useThemableLogos

--- frontend/src/utils/logos.ts
+++ frontend/src/utils/logos.ts
@@ -4,16 +4,14 @@ import { useTheme } from "../context/ThemeContext.tsx";
 import MiniLogoLight from "../assets/images/minilogo.svg";
 import MiniLogoDark from "../assets/images/dark_minilogo.svg";
 
-// Mapeamento dos assets
-const themeableAssets = {
+const themeableLogos = {
   minilogo: {
     light: MiniLogoLight,
     dark: MiniLogoDark,
   },
 };
 
-// Hook personalizado para obter o asset correto
-export const useThemeableAsset = (assetKey: keyof typeof themeableAssets) => {
+export const useThemeableLogos = (assetKey: keyof typeof themeableLogos) => {
   const { theme } = useTheme(); // Obtém o tema atual ('light' or 'dark')
-  return themeableAssets[assetKey][theme]; // Retorna a imagem correspondente
+  return themeableLogos[assetKey][theme]; // Retorna a imagem correspondente
 };
